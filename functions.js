e47d17f11467089968483d9f838b3b7ea31a87fb
Alberto Liguori <a1liguorialberto@gmail.com>
2020-12-10 00:30:13 +0100

    adds 6 sections draft

---
+++ functions.js
@@ -0,0 +1,48 @@
+let navListArray = ["Home", "Culinary History", "Our Team", "our Menu", "TakeOut", "Bulletin", "Reservsations", "Carrello"]
+
+let itemsArray = [
+  {
+    imgUrl:"css/img/sushi-1.png",
+    title:"The best table in town",
+    paragraph:"Sed aenean egestas ut aliquam turpis mauris, molestie. Vitae tellus tempor sem id tempus neque, tellus turpis turpis. Morbi tortor id gravida aliquet.",
+    buttonText:"EXPLORE THE MENU",
+
+  },
+
+  {
+    imgUrl:"css/img/sushi-2.png",
+    title:"Perfect For Groups",
+    paragraph:"Quam eu proin sit massa condimentum. Volutpat non pulvinar aliquet nunc. Orci elementum in aliquet a gravida vivamus aliquam turpis vitae.",
+    buttonText:"MAKE A RESERVATION",
+
+  },
+
+  {
+    imgUrl:"css/img/sushi-3.png",
+    title:"Fresh produce everyday",
+    paragraph:"Hendrerit amet, volutpat leo non, commodo maecenas scelerisque tincidunt. Morbi vulputate morbi purus quisque sit sagittis orci elementum gravida.",
+    buttonText:"LEARN MORE ABOUT US",
+
+  },
+]
+
+let newsArray=[
+  {
+    imgUrl:"css/img/best-coffee.jpg",
+    title:"THE BEST COFFEE IN TOWN",
+    details:["By admin" , "January 7th 2020", "Categories: News"]
+
+  },
+  {
+    imgUrl:"css/img/cook-girl.jpg",
+    title:"DISCOVER OUR NEW MENU",
+    details:["By admin" , "January 7th 2020", "Categories: News"]
+
+  },
+  {
+    imgUrl:"css/img/contactless.jpg",
+    title:"WE NOW ACCEPT SQUARE",
+    details:["By admin" , "January 7th 2020", "Categories: News"]
+
+  },
+]
--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let myApp = new Vue({
+  el:("#root"),
 
+  data:{
+    navArray:navListArray,
+    section1Items:itemsArray,
+    sectionNewsArray: newsArray,
+  },
+
+})
